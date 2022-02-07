12ada7097424c144625e37e3bc737aee52fa74bb
Shayan Talebian <shayantalebianwork@gmail.com>
2022-02-07 13:49:20 +0330

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -39,11 +39,11 @@ btnRoll.addEventListener('click', function () {
 });
 
 // Create guide alert
-if (screen.width < 1050) {
+if (screen.width < 1000) {
   body.style.display = 'none';
   alert('This website is only available for computers');
 }
-if (screen.height < 1050) {
+if (screen.height < 700) {
   body.style.display = 'none';
   alert('This website is only available for computers');
 }
