4d948ffc81e0cf94fb9d09e443ea8c191f04606b
Shayan Talebian <shayantalebianwork@gmail.com>
2022-02-10 09:28:22 +0330

    Create switch player method

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 // Selecting elements
+const player0EL = document.querySelector('.player--0');
+const player1EL = document.querySelector('.player--1');
 const body = document.querySelector('.body');
 const score0EL = document.querySelector('#score--0');
 const score1EL = document.getElementById('score--1');
@@ -16,7 +18,10 @@ const btnHold = document.querySelector('.btn--hold');
 score0EL.textContent = 0;
 score1EL.textContent = 0;
 diceEL.classList.add('hidden');
+
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 // Rolling dice functionality
 btnRoll.addEventListener('click', function () {
@@ -32,9 +37,15 @@ btnRoll.addEventListener('click', function () {
   if (dice !== 1) {
     // Add dice to current score
     currentScore += dice;
-    current0EL.textContent = currentScore;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
     //switch to next player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    currentScore = 0;
+    player0EL.classList.toggle('player--active');
+    player1EL.classList.toggle('player--active');
   }
 });
 
