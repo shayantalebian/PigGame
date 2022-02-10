259ed95c3e549c2ad641a849de5ae819e6159b23
Shayan Talebian <shayantalebianwork@gmail.com>
2022-02-10 12:57:54 +0330

    Create newGame button

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,15 +14,31 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
+let scores = '';
+let currentScore = '';
+let activePlayer = '';
+let playing = '';
 // Starting conditions
-score0EL.textContent = 0;
-score1EL.textContent = 0;
-diceEL.classList.add('hidden');
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0EL.textContent = 0;
+  score1EL.textContent = 0;
+  current0EL.textContent = 0;
+  current1EL.textContent = 0;
+
+  player0EL.classList.remove('player--active');
+  player1EL.classList.remove('player--active');
+  player1EL.classList.remove('player--winner');
+  player1EL.classList.remove('player--winner');
+  player0EL.classList.add('player--active');
+  diceEL.classList.add('hidden');
+};
+init();
 
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -31,7 +47,6 @@ const switchPlayer = function () {
   player0EL.classList.toggle('player--active');
   player1EL.classList.toggle('player--active');
 };
-
 // Rolling dice functionality
 btnRoll.addEventListener('click', function () {
   if (playing) {
@@ -80,6 +95,8 @@ btnHold.addEventListener('click', function () {
     }
   }
 });
+
+btnNew.addEventListener('click', init);
 
 // Create guide alert
 if (screen.width < 1000) {
